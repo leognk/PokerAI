2dd67abbdbdd97c86cdaca285c53d76d860d8c84
leognk <leotaisei@gmail.com>
2022-02-26 17:16:21 +0100

    removed preflop strat in blueprint nodes count

--- CountBlueprintNodes/CountBlueprintNodes.cpp
+++ CountBlueprintNodes/CountBlueprintNodes.cpp
@@ -12,19 +12,18 @@ int main()
 	std::vector<std::vector<abc::TreeTraverser::seq_t>> actionSeqs = traverser.traverseTree();
 
 	// Count total number of nodes including card abstraction.
-	uint64_t nNodesPreflop = bp::N_BCK_PREFLOP * actionSeqs[egn::PREFLOP].size();
-	uint64_t nNodesNoPreflop =
-		bp::N_BCK_FLOP * actionSeqs[egn::FLOP].size()
+	uint64_t nNodes =
+		bp::N_BCK_PREFLOP * actionSeqs[egn::PREFLOP].size()
+		+ bp::N_BCK_FLOP * actionSeqs[egn::FLOP].size()
 		+ bp::N_BCK_TURN * actionSeqs[egn::TURN].size()
 		+ bp::N_BCK_RIVER * actionSeqs[egn::RIVER].size();
-	uint64_t nNodes = nNodesPreflop + nNodesNoPreflop;
 
-	// Memory for the regrets and the preflop final strategy.
-	uint64_t ram = nNodes * sizeof(int32_t) + nNodesPreflop * sizeof(uint32_t);
+	// Memory for the regrets.
+	uint64_t ram = nNodes * sizeof(int32_t);
 
-	// Memory for the snapshots, the checkpoint, and the final strategy stored to the disk.
+	// Memory for the snapshots and the checkpoint stored to the disk.
 	uint64_t stratHdd = nNodes * sizeof(uint16_t);
-	uint64_t hdd = bp::nSnapshots * nNodesNoPreflop * sizeof(uint16_t) + ram + stratHdd;
+	uint64_t hdd = bp::nSnapshots * nNodes * sizeof(uint16_t) + ram + stratHdd;
 
 	const std::string duration = opt::prettyDuration(startTime);
 
