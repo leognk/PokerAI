80ea69b1e64b9481de3d30ba5097321ae0648d96
leognk <leotaisei@gmail.com>
2022-03-06 10:56:50 +0100

    use pretty print functions in GameEnginePerf

--- GameEnginePerformance/GameEnginePerf.cpp
+++ GameEnginePerformance/GameEnginePerf.cpp
@@ -1,47 +1,42 @@
 #include "../GameEngine/PlayGame.h"
 #include "../RandomAI/RandomAI.h"
-#include <iostream>
-#include <iomanip> 
-#include <chrono>
+#include "../Utils/Time.h"
+#include "../Utils/StringManip.h"
 
 int main()
 {
-    // Constants
-    // In seconds
-    const double maxDuration = 1;
+    const double endDuration = 1;
     const unsigned rngSeed = 1;
-    const egn::chips ante = 0, bigBlind = 10;
+    const egn::chips ante = 0;
+    const egn::chips bigBlind = 10;
     const egn::chips stake = 1000;
-    const double foldProba = 1. / 3;
-    const double callProba = 1. / 3;
+    const double foldProba = 1.0 / 3;
+    const double callProba = 1.0 / 3;
 
-
-    // Define game variables.
     std::array<egn::chips, egn::MAX_PLAYERS> stakes{
         stake, stake, stake, stake, stake, stake };
+
     opt::RandomAI randomAI(foldProba, callProba, rngSeed);
+
     std::array<egn::Player*, egn::MAX_PLAYERS> players{
         &randomAI, &randomAI, &randomAI, &randomAI, &randomAI, &randomAI };
+
     egn::PlayGame play(ante, bigBlind, stakes, players, 0, rngSeed);
 
     // Simulate games.
+    const opt::time_t startTime = opt::getTime();
+    double currDuration = 0;
     uint64_t gameCount = 0;
-    double duration = 0;
-    auto t1 = std::chrono::high_resolution_clock::now();
-    while (duration < maxDuration) {
+    while (currDuration < endDuration) {
         play.playAndReset();
-        ++gameCount;
-        if (gameCount % uint64_t(1e3) == 0) {
-            auto t2 = std::chrono::high_resolution_clock::now();
-            duration = 1e-9 * std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count();
-        }
+        if (++gameCount % uint64_t(1e3) == 0)
+            currDuration = opt::getDuration(startTime);
     }
 
-    // Print results.
-    std::cout << (uint64_t)std::round(duration) << " s" << std::endl;
-    std::cout << std::fixed << std::setprecision(1);
-    std::cout << 1e-6 * gameCount << " Mgames" << std::endl;
-    std::cout << (uint64_t)std::round(1e9 * duration / gameCount) << " nanosec/game" << std::endl;
-    std::cout << std::setprecision(2);
-    std::cout << 1e-6 * gameCount / duration << " Mgame/s" << std::endl;
+	// Print stats.
+    std::cout
+        << opt::prettyNumDg(currDuration, 3, true) << "s\n"
+        << opt::prettyNumDg(gameCount, 3, true) << "games\n"
+        << opt::prettyNumDg(gameCount / currDuration, 3, true) << "games/s\n"
+        << opt::prettyNumDg(currDuration / gameCount, 3, true) << "s/game\n";
 }
